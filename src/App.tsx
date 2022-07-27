497f3ac06db537eb9d91184bdef8d054ca9e73b6
Rahad Khan <clash146098@gmail.com>
2022-07-27 14:27:26 +0600

    coolumn design done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Heaader from './component/Header/Heaader';
+import WritingArea from './component/Home/WritingAss/WritingArea';
+import WritingAss from './component/Home/WritingAss/WritingAss';
 
 function App() {
   return (
     <div>
       <Heaader />
+      <WritingAss />
+      <WritingArea />
     </div>
   );
 }
---
+++ src/component/Home/WritingAss/WritingAss.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const WritingAss = () => {
+    return (
+        <section>
+            <div className='d-flex justify-content-between align-items-center'>
+                <div className='fw-bolder'><h4>Home/Writing Assistant</h4></div>
+                <div>
+                    <button className='border border-0 bg-primary text-white px-5 py-2'>Save</button>
+                    <button className='border border-0 bg-primary ms-4 text-white px-5 py-2 d-none d-lg-inline-block'>Filter</button>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default WritingAss;
